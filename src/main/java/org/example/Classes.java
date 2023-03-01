9ea3c019c88268abce07b3b00a00827f90ae9cf3
NitinSinghR <nitin.singh@tringapps.com>
2023-03-01 16:13:07 +0530

    Add files via upload

--- src/main/java/org/example/Classes.java
+++ src/main/java/org/example/Classes.java
@@ -7,7 +7,7 @@ import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Classes {
+class Classes {
     static Scanner sc = new Scanner(System.in);
     static Logger l = Logger.getLogger("com.api.jar");
 
